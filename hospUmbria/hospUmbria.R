671ab1cc718de87325895e8deb61f4597224dcd6
vi-enne <vittorio.nicoletta.1@ulaval.ca>
2022-03-23 10:18:08 -0400

    Update hospUmbria.R

--- hospUmbria/hospUmbria.R
+++ hospUmbria/hospUmbria.R
@@ -10,29 +10,29 @@ umbria <- data[data$denominazione_regione == "Umbria", c("data", "ricoverati_con
 umbria$data <- as.Date(umbria$data)
 umbria <- umbria[umbria$data >= "2021-01-31",]
 
-umbria$note <- sub(".*i fa presente che ", "", umbria$note) 
-umbria$note <- sub(".*i fa presente come ", "", umbria$note) 
-umbria$note <- sub(" dei ricoveri.*", "", umbria$note) 
-umbria$note[umbria$data=="2021-02-02"] <- 45
-umbria$note[umbria$data=="2021-03-22"] <- 7
-umbria$note[umbria$data=="2021-07-06"] <- 1
-umbria$note[umbria$data=="2021-12-24"] <- 0
-umbria$note[umbria$data=="2022-01-13"] <- 5
+umbria$noteClean <- sub(".*i fa presente che ", "", umbria$note) 
+umbria$noteClean <- sub(".*i fa presente come ", "", umbria$noteClean) 
+umbria$noteClean <- sub(" dei ricoveri.*", "", umbria$noteClean) 
+umbria$noteClean[umbria$data=="2021-02-02"] <- 45
+umbria$noteClean[umbria$data=="2021-03-22"] <- 7
+umbria$noteClean[umbria$data=="2021-07-06"] <- 1
+umbria$noteClean[umbria$data=="2021-12-24"] <- 0
+umbria$noteClean[umbria$data=="2022-01-13"] <- 5
 
 
 temp <- qdapRegex::ex_between(umbria$note[umbria$data>="2022-01-25"], "i fa presente che ", " dei ricoveri")
-umbria$note[umbria$data>="2022-01-25"] <- unlist(lapply(temp, function(x) sum(as.numeric(x))))
+umbria$noteClean[umbria$data>="2022-01-25"] <- unlist(lapply(temp, function(x) sum(as.numeric(x))))
 
-umbria$note <- sub("uno", "1", umbria$note) 
-umbria$note <- sub("due", "2", umbria$note) 
-umbria$note <- sub("tre", "3", umbria$note) 
+umbria$noteClean <- sub("uno", "1", umbria$noteClean) 
+umbria$noteClean <- sub("due", "2", umbria$noteClean) 
+umbria$noteClean <- sub("tre", "3", umbria$noteClean) 
 
-umbria$note <- as.numeric(umbria$note)
-umbria$note[is.na(umbria$note)] <- 0
+umbria$noteClean <- as.numeric(umbria$noteClean)
+umbria$noteClean[is.na(umbria$noteClean)] <- 0
 
-umbria$ricoverati_AM_covid <- umbria$ricoverati_con_sintomi - umbria$note
+umbria$ricoverati_AM_covid <- umbria$ricoverati_con_sintomi - umbria$noteClean
 
-umbriaPlot <- umbria[, c("data", "ricoverati_AM_covid", "note")]
+umbriaPlot <- umbria[, c("data", "ricoverati_AM_covid", "noteClean")]
 colnames(umbriaPlot)[2] <- "Ricoverati positivi NON TI in discipline di Area Medica (qui intesi PER Covid-19)"
 colnames(umbriaPlot)[3] <- "Ricoverati positivi NON TI in discipline NON di Area Medica (qui intesi CON Covid-19)"
 
