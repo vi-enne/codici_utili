b087edcffcb515130afdf8b79fd54d1e4f0e1ab6
vi-enne <vittorio.nicoletta.1@ulaval.ca>
2024-07-31 15:26:31 -0400

    Create estraiPiattaformaFirme.R

---
+++ piattaformaFirme/estraiPiattaformaFirme.R
@@ -0,0 +1,10 @@
+library(jsonlite)
+
+jsonl_file <- "https://raw.githubusercontent.com/ondata/liberiamoli-tutti/main/referendum_iniziative_popolare/data/referendum_iniziative_popolare.jsonl"
+
+df   <- stream_in(file(jsonl_file))
+df$tipo <- ifelse(df$quorum < 5*10^5, "Legge di iniziativa popolare", "Referendum abrogativo")
+df$url <- paste0("[",df$url,"](", df$url, ")")
+df$sito <- paste0("[",df$sito,"](", df$sito, ")")
+
+write.csv(df, "piattaformaFirme/firmeReferendum.csv", row.names = F)
