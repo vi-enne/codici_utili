095b778e983ef452d7ca948ca9e1e192e321389c
vi-enne <44818674+vi-enne@users.noreply.github.com>
2024-08-05 12:42:40 -0400

    Update estraiPiattaformaFirme.R

--- piattaformaFirme/estraiPiattaformaFirme.R
+++ piattaformaFirme/estraiPiattaformaFirme.R
@@ -7,5 +7,8 @@ df$tipo <- ifelse(df$quorum < 5*10^5, "Legge di iniziativa popolare", "Referendu
 df$url <- paste0("[",df$url,"](", df$url, ")")
 df$sito <- paste0("[",df$sito,"](", df$sito, ")")
 df$percentualeQuorum <- paste0(round(df$sostenitori/df$quorum * 100,2), "%")
+temp <- df$titoloLeggeCostituzionale[df$id == 500013]
+df$titoloLeggeCostituzionale[df$id == 500013] <- df$titolo[df$id == 500013]
+df$titolo[df$id == 500013] <- temp
 
 write.csv(df, "piattaformaFirme/firmeReferendum.csv", row.names = F)
