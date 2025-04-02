5b92d999fcf6459b9365de66cc3d59b5f17af287
vi-enne <vittorio.nicoletta.1@ulaval.ca>
2025-04-02 13:56:12 -0400

    Update estraiPiattaformaFirme.R

--- piattaformaFirme/estraiPiattaformaFirme.R
+++ piattaformaFirme/estraiPiattaformaFirme.R
@@ -5,6 +5,7 @@ jsonl_file <- "https://raw.githubusercontent.com/ondata/liberiamoli-tutti/main/r
 df   <- stream_in(file(jsonl_file))
 df$tipo <- ifelse(df$quorum < 5*10^5, "Legge di iniziativa popolare", "Referendum abrogativo")
 df$url <- gsub("pnri.", "", df$url)
+df$id <- paste0("[",df$id,"](", df$url, ")")
 df$url <- paste0("[",df$url,"](", df$url, ")")
 df$sito <- paste0("[",df$sito,"](", df$sito, ")")
 df$percentualeQuorum <- paste0(round(df$sostenitori/df$quorum * 100,2), "%")
