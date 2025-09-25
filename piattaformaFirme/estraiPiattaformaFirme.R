33caded04b785bba98f6fd2df2596e4435153bcb
vi-enne <44818674+vi-enne@users.noreply.github.com>
2025-09-25 10:21:20 -0400

    Update estraiPiattaformaFirme.R

--- piattaformaFirme/estraiPiattaformaFirme.R
+++ piattaformaFirme/estraiPiattaformaFirme.R
@@ -1,5 +1,5 @@
 library(jsonlite)
-dataPatch <- T
+dataPatch <- F
 jsonl_file <-
   "https://raw.githubusercontent.com/ondata/liberiamoli-tutti/main/referendum_iniziative_popolare/data/referendum_iniziative_popolare.jsonl"
 
